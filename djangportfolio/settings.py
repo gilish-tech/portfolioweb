58b074335b802028b05a7cffcd360c8c28606ee4
GILFROSH <nwabuezegilbert59@gmail.com>
2023-04-20 01:29:41 +0100

    first commit

--- djangportfolio/settings.py
+++ djangportfolio/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("PRIVATE_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
